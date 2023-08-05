2a3b743bdd8b0b9c36d72912c39635eb72969936
softacoder <113185323+softacoder@users.noreply.github.com>
2023-08-05 18:23:09 +0100

    commit

--- chat-server-react/src/App.js
+++ chat-server-react/src/App.js
@@ -1,25 +1,709 @@
-import logo from './logo.svg';
-import './App.css';
+// version9
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import ChatInterface from "./ChatInterface";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
+  const [data, setData] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  const fetchLatestMessages = async () => {
+    try {
+      const res = await axios.get(
+        "https://jan-softa-cyf-chat-server.glitch.me/messages/latest"
+      );
+      setData(res.data);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const fetchMessagesBySearch = async () => {
+    try {
+      const res = await axios.get(
+        `https://jan-softa-cyf-chat-server.glitch.me/messages/search?text=${searchText}`
+      );
+      setData(res.data);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchLatestMessages();
+  }, []);
+
+  const handleSearch = () => {
+    if (searchText.trim() === "") return;
+    setIsLoading(true);
+    setError("");
+    fetchMessagesBySearch();
+  };
+
+  const handleDeleteMessage = () => {
+    const idToDelete = document.getElementById("deleteId").value;
+    if (!idToDelete || isNaN(idToDelete)) {
+      alert("Please enter a valid ID to delete.");
+      return;
+    }
+
+    // const url = `/messages/delete/${idToDelete}`;
+    const url = `https://jan-softa-cyf-chat-server.glitch.me/messages/delete/${idToDelete}`;
+    axios
+      .delete(url)
+      .then((response) => {
+        if (!response.data.success) {
+          throw new Error("Error deleting message");
+        }
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.error("Error deleting message:", error);
+      });
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>CYF Chat App</h1>
+        <div className="chat-container">
+          <div className="chat-messages">
+            <input
+              type="text"
+              placeholder="Search messages..."
+              value={searchText}
+              onChange={(e) => setSearchText(e.target.value)}
+            />
+            <button onClick={handleSearch}>Search</button>
+            <ul>
+              {data.map((message) => (
+                <li key={message.id}>
+                  <strong>{message.from}:</strong> {message.text}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="chat-interface">
+            <ChatInterface onDeleteMessage={handleDeleteMessage} />
+          </div>
+        </div>
       </header>
     </div>
   );
 }
 
 export default App;
+
+// version8
+// import React, { useEffect, useState } from "react";
+// import "./App.css";
+// import axios from "axios";
+// import ChatInterface from "./ChatInterface";
+
+// function App() {
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [error, setError] = useState("");
+//   const [data, setData] = useState([]);
+//   const [searchText, setSearchText] = useState(""); // State for search input
+
+//   const fetchLatestMessages = async () => {
+//     try {
+//       const res = await axios.get(
+//         "https://jan-softa-cyf-chat-server.glitch.me/messages/latest"
+//       );
+//       setData(res.data);
+//     } catch (error) {
+//       setError(error.message);
+//     } finally {
+//       setIsLoading(false);
+//     }
+//   };
+
+//   const fetchMessagesBySearch = async () => {
+//     try {
+//       const res = await axios.get(
+//         `https://jan-softa-cyf-chat-server.glitch.me/messages/search?text=${searchText}`
+//       );
+//       setData(res.data);
+//     } catch (error) {
+//       setError(error.message);
+//     } finally {
+//       setIsLoading(false);
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchLatestMessages();
+//   }, []);
+
+//   const handleSearch = () => {
+//     if (searchText.trim() === "") return;
+//     setIsLoading(true);
+//     setError("");
+//     fetchMessagesBySearch();
+//   };
+
+//   const handleDeleteMessage = () => {
+//     const idToDelete = document.getElementById("deleteId").value;
+//     if (!idToDelete || isNaN(idToDelete)) {
+//       alert("Please enter a valid ID to delete.");
+//       return;
+//     }
+
+//     const url = `/messages/delete/${idToDelete}`;
+//     axios
+//       .delete(url)
+//       .then((response) => {
+//         if (!response.data.success) {
+//           throw new Error("Error deleting message");
+//         }
+//         // Refresh the page to see the updated messages list
+//         window.location.reload();
+//       })
+//       .catch((error) => {
+//         console.error("Error deleting message:", error);
+//       });
+//   };
+
+//   if (isLoading) {
+//     return <div>Loading...</div>;
+//   }
+
+//   if (error) {
+//     return <div>Error: {error}</div>;
+//   }
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h1>CYF Chat App</h1>
+//         <div className="chat-container">
+//           <div className="chat-messages">
+//             <input
+//               type="text"
+//               placeholder="Search messages..."
+//               value={searchText}
+//               onChange={(e) => setSearchText(e.target.value)}
+//             />
+//             <button onClick={handleSearch}>Search</button>
+//             <ul>
+//               {data.map((message) => (
+//                 <li key={message.id}>
+//                   <strong>{message.from}:</strong> {message.text}
+//                 </li>
+//               ))}
+//             </ul>
+//           </div>
+//           <div className="chat-interface">
+//             <ChatInterface onDeleteMessage={handleDeleteMessage} />
+//           </div>
+//         </div>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// version7 it has no html content
+// import React, { useEffect, useState } from "react";
+// import "./App.css";
+// import axios from "axios";
+// import ChatInterface from "./ChatInterface"; // Import the new component
+
+// function App() {
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [error, setError] = useState("");
+//   const [data, setData] = useState([]);
+//   const [searchText, setSearchText] = useState(""); // State for search input
+
+//   const fetchLatestMessages = async () => {
+//     try {
+//       const res = await axios.get(
+//         "https://jan-softa-cyf-chat-server.glitch.me/messages/latest"
+//       );
+//       setData(res.data);
+//     } catch (error) {
+//       setError(error.message);
+//     } finally {
+//       setIsLoading(false);
+//     }
+//   };
+
+//   const fetchMessagesBySearch = async () => {
+//     try {
+//       const res = await axios.get(
+//         `https://jan-softa-cyf-chat-server.glitch.me/messages/search?text=${searchText}`
+//       );
+//       setData(res.data);
+//     } catch (error) {
+//       setError(error.message);
+//     } finally {
+//       setIsLoading(false);
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchLatestMessages();
+//   }, []);
+
+//   const handleSearch = () => {
+//     if (searchText.trim() === "") return;
+//     setIsLoading(true);
+//     setError("");
+//     fetchMessagesBySearch();
+//   };
+
+//   const handleDeleteMessage = () => {
+//     const idToDelete = document.getElementById("deleteId").value;
+//     if (!idToDelete || isNaN(idToDelete)) {
+//       alert("Please enter a valid ID to delete.");
+//       return;
+//     }
+
+//     const url = `/messages/delete/${idToDelete}`;
+//     axios
+//       .delete(url)
+//       .then((response) => {
+//         if (!response.data.success) {
+//           throw new Error("Error deleting message");
+//         }
+//         // Refresh the page to see the updated messages list
+//         window.location.reload();
+//       })
+//       .catch((error) => {
+//         console.error("Error deleting message:", error);
+//       });
+//   };
+
+//   if (isLoading) {
+//     return <div>Loading...</div>;
+//   }
+
+//   if (error) {
+//     return <div>Error: {error}</div>;
+//   }
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <input
+//           type="text"
+//           placeholder="Search messages..."
+//           value={searchText}
+//           onChange={(e) => setSearchText(e.target.value)}
+//         />
+//         <button onClick={handleSearch}>Search</button>
+//         <ul>
+//           {data.map((message) => (
+//             <li key={message.id}>
+//               <strong>{message.from}:</strong> {message.text}
+//             </li>
+//           ))}
+//         </ul>
+//         <ChatInterface onDeleteMessage={handleDeleteMessage} />
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// version6
+// import React, { useEffect, useState } from "react";
+// import "./App.css";
+// import axios from "axios";
+
+// function App() {
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [error, setError] = useState("");
+//   const [data, setData] = useState([]);
+//   const [searchText, setSearchText] = useState(""); // State for search input
+
+//   const fetchLatestMessages = async () => {
+//     try {
+//       const res = await axios.get(
+//         "https://jan-softa-cyf-chat-server.glitch.me/messages/latest"
+//       );
+//       setData(res.data);
+//     } catch (error) {
+//       setError(error.message);
+//     } finally {
+//       setIsLoading(false);
+//     }
+//   };
+
+//   const fetchMessagesBySearch = async () => {
+//     try {
+//       const res = await axios.get(
+//         `https://jan-softa-cyf-chat-server.glitch.me/messages/search?text=${searchText}`
+//       );
+//       setData(res.data);
+//     } catch (error) {
+//       setError(error.message);
+//     } finally {
+//       setIsLoading(false);
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchLatestMessages();
+//   }, []);
+
+//   const handleSearch = () => {
+//     if (searchText.trim() === "") return;
+//     setIsLoading(true);
+//     setError("");
+//     fetchMessagesBySearch();
+//   };
+
+//   if (isLoading) {
+//     return <div>Loading...</div>;
+//   }
+
+//   if (error) {
+//     return <div>Error: {error}</div>;
+//   }
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <input
+//           type="text"
+//           placeholder="Search messages..."
+//           value={searchText}
+//           onChange={(e) => setSearchText(e.target.value)}
+//         />
+//         <button onClick={handleSearch}>Search</button>
+//         <ul>
+//           {data.map((message) => (
+//             <li key={message.id}>
+//               <strong>{message.from}:</strong> {message.text}
+//             </li>
+//           ))}
+//         </ul>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// version5
+// import React, { useEffect, useState } from "react";
+// // import logo from "./logo.svg";
+// import "./App.css";
+// import axios from "axios";
+
+// function App() {
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [error, setError] = useState("");
+//   const [data, setData] = useState([]);
+
+//   const fetchLatestMessages = async () => {
+//     try {
+//       const res = await axios.get(
+//         "https://jan-softa-cyf-chat-server.glitch.me/messages/latest"
+//       );
+//       setData(res.data);
+//     } catch (error) {
+//       setError(error.message);
+//     } finally {
+//       setIsLoading(false);
+//     }
+//   };
+
+//   const fetchMessagesBySearch = async (searchText) => {
+//     try {
+//       const res = await axios.get(
+//         `https://jan-softa-cyf-chat-server.glitch.me/messages/search?text=${searchText}`
+//       );
+//       setData(res.data);
+//     } catch (error) {
+//       setError(error.message);
+//     } finally {
+//       setIsLoading(false);
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchLatestMessages();
+//   }, []);
+
+//   if (isLoading) {
+//     return <div>Loading...</div>;
+//   }
+
+//   if (error) {
+//     return <div>Error: {error}</div>;
+//   }
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         {/* <img src={logo} className="App-logo" alt="logo" /> */}
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//         {/* Display fetched data */}
+//         <ul>
+//           {data.map((message) => (
+//             <li key={message.id}>
+//               <strong>{message.from}:</strong> {message.text}
+//             </li>
+//           ))}
+//         </ul>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// version4
+// import React, { useEffect, useState } from "react";
+// import logo from "./logo.svg";
+// import "./App.css";
+// import axios from "axios";
+
+// function App() {
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [error, setError] = useState("");
+//   const [data, setData] = useState([]);
+
+//   const fetchApi = async () => {
+//     try {
+//       const res = await axios.get(
+//         "https://jan-softa-cyf-chat-server.glitch.me/messages/latest"
+//       );
+//       setData(res.data);
+//     } catch (error) {
+//       setError(error.message);
+//     } finally {
+//       setIsLoading(false);
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchApi();
+//   }, []);
+
+//   if (isLoading) {
+//     return <div>Loading...</div>;
+//   }
+
+//   if (error) {
+//     return <div>Error: {error}</div>;
+//   }
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//         <ul>
+//           {data.map((message) => (
+//             <li key={message.id}>
+//               <strong>{message.from}:</strong> {message.text}
+//             </li>
+//           ))}
+//         </ul>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// version3
+// import React, { useEffect, useState } from "react";
+// // import logo from "./logo.svg";
+// import "./App.css";
+// import axios from "axios";
+
+// function App() {
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [error, setError] = useState("");
+//   const [data, setData] = useState([]);
+
+//   const fetchApi = async () => {
+//     try {
+//       // const res = await axios.get("http://localhost:9090/messages");
+//       const res = await axios.get(
+//         "https://jan-softa-cyf-chat-server.glitch.me/messages"
+//       );
+//       setData(res.data);
+//     } catch (error) {
+//       setError(error.message);
+//     } finally {
+//       setIsLoading(false);
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchApi();
+//   }, []);
+
+//   if (isLoading) {
+//     return <div>Loading...</div>;
+//   }
+
+//   if (error) {
+//     return <div>Error: {error}</div>;
+//   }
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//         <ul>
+//           {data.map((message) => (
+//             <li key={message.id}>
+//               <strong>{message.from}:</strong> {message.text}
+//             </li>
+//           ))}
+//         </ul>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// version2
+// import React, { useEffect, useState } from "react";
+// import logo from "./logo.svg";
+// import "./App.css";
+// import axios from "axios";
+
+// function App() {
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [error, setError] = useState("");
+//   const [data, setData] = useState([]);
+
+//   const fetchApi = async () => {
+//     try {
+//       const res = await axios.get("http://localhost:9090/messages");
+//       const res = await axios.get(
+//         "https://jan-softa-cyf-chat-server.glitch.me"
+//       );
+//       setData(res.data);
+//     } catch (error) {
+//       setError(error.message);
+//     } finally {
+//       setIsLoading(false);
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchApi();
+//   }, []);
+
+//   if (isLoading) {
+//     return <div>Loading...</div>;
+//   }
+
+//   if (error) {
+//     return <div>Error: {error}</div>;
+//   }
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//         { }
+//         <ul>
+//           {data.map((message) => (
+//             <li key={message.id}>
+//               <strong>{message.from}:</strong> {message.text}
+//             </li>
+//           ))}
+//         </ul>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// original
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ chat-server-react/src/ChatInterface.js
@@ -0,0 +1,201 @@
+// version2
+import React from "react";
+import axios from "axios";
+
+function ChatInterface({ onDeleteMessage }) {
+  const handleDelete = () => {
+    onDeleteMessage();
+  };
+
+  const handleSendMessage = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const from = formData.get("from");
+    const text = formData.get("text");
+
+    try {
+      await axios.post("https://jan-softa-cyf-chat-server.glitch.me/messages", {
+        from,
+        text,
+      });
+      window.location.reload();
+    } catch (error) {
+      console.error("Error sending message:", error);
+    }
+  };
+
+  return (
+    <div className="chat-interface">
+      <h2>Send a message</h2>
+      <form onSubmit={handleSendMessage}>
+        <p>
+          Name: <input type="text" name="from" placeholder="Your Name" /> <br />
+          Message:{" "}
+          <input type="text" name="text" placeholder="The message..." />
+          <br />
+        </p>
+        <button type="submit">Send</button>
+      </form>
+      <br />
+      <a href="/messages">See all messages</a>
+
+      {/* Delete Message Form */}
+      <form>
+        <p>
+          Delete Message:{" "}
+          <input
+            type="number"
+            id="deleteId"
+            name="id"
+            placeholder="Delete message by ID..."
+          />
+        </p>
+        <button type="button" onClick={handleDelete}>
+          Submit
+        </button>
+      </form>
+
+      <style jsx>
+        {`
+          .chat-interface {
+            background-color: #f9f9f9;
+            padding: 20px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            text-align: center;
+          }
+
+          h2 {
+            color: #555;
+          }
+
+          form {
+            margin-top: 20px;
+          }
+
+          input[type="text"] {
+            padding: 5px;
+            margin-bottom: 10px;
+            width: 100%;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+          }
+
+          button {
+            padding: 8px 15px;
+            background-color: #007bff;
+            color: #fff;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+          }
+
+          a {
+            text-decoration: none;
+            color: #007bff;
+            display: block;
+            margin-top: 10px;
+          }
+        `}
+      </style>
+    </div>
+  );
+}
+
+export default ChatInterface;
+
+// original
+// import React from "react";
+
+// function ChatInterface({ onDeleteMessage }) {
+//   const handleDelete = () => {
+//     onDeleteMessage();
+//   };
+
+//   return (
+//     <div className="chat-interface">
+//       <h1>CYF Chat</h1>
+//       <h2>Send a message</h2>
+//       <form action="/messages" method="post">
+//         <p>
+//           Name: <input type="text" name="from" placeholder="Your Name" /> <br />
+//           Message:{" "}
+//           <input type="text" name="text" placeholder="The message..." />
+//           <br />
+//         </p>
+//         <button type="submit">Send</button>
+//       </form>
+//       <br />
+//       <a href="/messages">See all messages</a>
+
+//       {/* Delete Message Form */}
+//       <form>
+//         <p>
+//           Delete Message:{" "}
+//           <input
+//             type="number"
+//             id="deleteId"
+//             name="id"
+//             placeholder="Delete message by ID..."
+//           />
+//         </p>
+//         <button type="button" onClick={handleDelete}>
+//           Submit
+//         </button>
+//       </form>
+
+//       <style jsx>
+//         {`
+//           .chat-interface {
+//             // background-color: #f9f9f9;
+//             padding: 20px;
+//             border: 1px solid #ccc;
+//             border-radius: 5px;
+//             box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+//             text-align: center;
+//           }
+
+//           h1 {
+//             color: #333;
+//           }
+
+//           h2 {
+//             color: #555;
+//           }
+
+//           form {
+//             margin-top: 20px;
+//           }
+
+//           input[type="text"] {
+//             padding: 5px;
+//             margin-bottom: 10px;
+//             width: 100%;
+//             border: 1px solid #ccc;
+//             border-radius: 4px;
+//           }
+
+//           button {
+//             padding: 8px 15px;
+//             background-color: #007bff;
+//             color: #fff;
+//             border: none;
+//             border-radius: 4px;
+//             cursor: pointer;
+//           }
+
+//           a {
+//             text-decoration: none;
+//             color: #007bff;
+//             display: block;
+//             margin-top: 10px;
+//           }
+//         `}
+//       </style>
+//     </div>
+//   );
+// }
+
+// export default ChatInterface;
